4d9fdcb708d042a67851da38e8c30047c27a9dd9
shailesh <saileshtiwari71@gmail.com>
2021-10-14 10:03:18 +0530

    changed.

--- app/src/main/java/com/example/sample/SplashActivity.kt
+++ app/src/main/java/com/example/sample/SplashActivity.kt
@@ -17,7 +17,6 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun moveToLoginActivity() {
-
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
